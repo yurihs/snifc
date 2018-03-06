eeea8472299162ee701374ac3210da0af205bde1
yurihs <yurisalvador@hotmail.com>
2018-03-06 14:37:17 -0300

    Commit inicial

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+version = "0.0.1"
+
+requirements = [
+]
+
+setup(
+    name="snifc",
+    version=version,
+    description="Sniffer de pacotes",
+    author="yurihs",
+    packages=["snifc"],
+    packagedir={"snifc": "snifc"},
+    include_package_data=True,
+    install_requires=requirements,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ]
+)
